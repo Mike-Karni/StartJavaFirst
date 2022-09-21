3e4d536f5b3351fcdd00a8259ea69504a530922a
Mike <3923499@mail.ru>
2022-09-21 19:31:42 +0300

    Done!

---
+++ Lesson2/Wolf.java
@@ -0,0 +1,33 @@
+public class Wolf {
+
+    String sex = "man";
+    String nickName = "Grey Power";
+    int weight = 47;
+    int age = 10;
+    String color = "grey";
+
+    void walk() {
+        System.out.println("Серый волк крадётся за добычей.");
+    }
+
+    void sit() {
+        System.out.println("Серый волк сидит в засаде на зверя.");
+    }
+
+    void run() {
+        System.out.println("Серый волк бежит за охотником.");
+    }
+
+    void howl() {
+        System.out.println("Серый волк воет на Луну!");
+    }
+
+    void hunt() {
+        System.out.println("Серый волк охотится только когда голоден.");
+    }
+
+
+
+
+
+}
