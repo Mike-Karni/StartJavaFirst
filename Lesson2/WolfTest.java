c06c88fe86629f097a5a20f0718d6610ae70083a
Mike-Karni <102918952+Mike-Karni@users.noreply.github.com>
2022-09-25 21:59:37 +0300

    Update WolfTest.java

--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -2,12 +2,6 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        // // wolfOne.age = 12;
-        // System.out.println("Возраст волка = " + wolfOne.age + " лет.");
-        // // wolfOne.weight = 55;
-        // System.out.println("Волчок то наш растолстел и весит уже " + wolfOne.weight + " кг.");
-        // // wolfOne.nickName = "King Wolf";
-        // System.out.println("Волк стал вожаком стаи, и теперь его кличка " + wolfOne.nickName);
         wolfOne.walk();
         wolfOne.sit();
         wolfOne.run();
